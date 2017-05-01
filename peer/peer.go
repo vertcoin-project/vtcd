92333eaaf170281d26e7417d5422e0daa4cfa32a
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-05-01 16:28:43 -0700

    peer: use testnet4 by default when creating peers

--- peer/peer.go
+++ peer/peer.go
@@ -2152,7 +2152,7 @@ func newPeerBase(origCfg *Config, inbound bool) *Peer {
 
 	// Set the chain parameters to testnet if the caller did not specify any.
 	if cfg.ChainParams == nil {
-		cfg.ChainParams = &chaincfg.TestNet3Params
+		cfg.ChainParams = &chaincfg.TestNet4Params
 	}
 
 	p := Peer{
